c4d33f94cafbebdb512f2e11d04247f61df12406
a0950088 <a0950088@gmail.com>
2023-07-07 23:14:42 +0800

    fixed main

--- main.py
+++ main.py
@@ -16,32 +16,32 @@ video_path = f"{CWD}/WarriorTwoPose/Video/detect/test3.mp4"
 warriorII = WarriorIIPosture()
 warriorII.sample(sample_path, storage_path)
 
-# if not warriorII.initialDetect():
-#     print("Please check sample file exist, or run sample function.")
-#     exit()
+if not warriorII.initialDetect():
+    print("Please check sample file exist, or run sample function.")
+    exit()
 
 # detect image
-# for idx, file in enumerate(IMAGE_FILES):
-#     image = cv2.imread(file)
-#     image_height, image_width, _ = image.shape
-#     frame = warriorII.detect(image, image_width, image_height, True)
-#     cv2.imshow('image',frame)
-#     cv2.waitKey(0)
+for idx, file in enumerate(IMAGE_FILES):
+    image = cv2.imread(file)
+    image_height, image_width, _ = image.shape
+    frame = warriorII.detect(image, image_width, image_height, True)
+    cv2.imshow('image',frame)
+    cv2.waitKey(0)
     
-# cap = cv2.VideoCapture(video_path)
-# original_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# original_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# print(original_width,original_height)
-# fps = cap.get(cv2.CAP_PROP_FPS)
-# while True:
-#     ret, frame = cap.read()
-#     if not ret:
-#         print("Video end")
-#         break
-#     frame = warriorII.detect(frame, original_width, original_height, False)
-#     print(warriorII.tips)
-#     cv2.imshow('image',frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
+cap = cv2.VideoCapture(video_path)
+original_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+original_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(original_width,original_height)
+fps = cap.get(cv2.CAP_PROP_FPS)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Video end")
+        break
+    frame = warriorII.detect(frame, original_width, original_height, False)
+    print(warriorII.tips)
+    cv2.imshow('image',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
